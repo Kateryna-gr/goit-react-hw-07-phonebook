26bf57ac4d2f7056ae27512dd1b7a4ed1c0eace9
Kateryna-gr <128936446+Kateryna-gr@users.noreply.github.com>
2023-11-12 01:05:32 +0200

    start

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -3,17 +3,16 @@ export const Filter = ({ onFilteredName, onFilteredNumber }) => {
     <div>
       <p>Find contacts by name</p>
       <input
-      type="text"
-      name="filterName"
-      onChange={evt => onFilteredName(evt.target.value)}
-    />
-    <p>Find contacts by number</p>
+        type="text"
+        name="filterName"
+        onChange={evt => onFilteredName(evt.target.value)}
+      />
+      <p>Find contacts by number</p>
       <input
-      type="number"
-      
-      name="filterNumber"
-      onChange={evt => onFilteredNumber(evt.target.value)}
-    />
+        type="number"
+        name="filterNumber"
+        onChange={evt => onFilteredNumber(evt.target.value)}
+      />
     </div>
   );
 };
